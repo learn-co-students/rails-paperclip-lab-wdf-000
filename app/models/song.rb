411ea0e7cc17e40c30f416efa07067a1bbd6e5f0
Ekaterina Petushova <epetushova@gmail.com>
2017-02-15 21:34:00 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,12 +1,14 @@
 class Song < ActiveRecord::Base
-  belongs_to :artist
+  	belongs_to :artist
+  	has_attached_file :album_cover, styles: { thumb: "100x100"}
+  	validates_attachment_content_type :album_cover, content_type: /\Aimage\/.*\z/
 
-  def artist_name
-    self.try(:artist).try(:name)
-  end
+  	def artist_name
+    	self.try(:artist).try(:name)
+  	end
 
-  def artist_name=(name)
-    artist = Artist.find_or_create_by(name: name)
-    self.artist = artist
-  end
+	def artist_name=(name)
+	    artist = Artist.find_or_create_by(name: name)
+	    self.artist = artist
+	end
 end
